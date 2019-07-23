1fb1c031575dcb621e92da54419af774227bcba9
chengcl1994 <chengcl1994@gmail.com>
2019-07-23 08:46:37 +0800

    changing distortion mode feature

---
+++ ZOSCad.py
@@ -0,0 +1,7 @@
+from ZOSCOM import *
+
+class ZOSCad(object):
+    def __init__(self):
+        pass
+    def export(self):
+        pass
---
+++ ZOSMCE.py
@@ -0,0 +1,14 @@
+from ZOSCOM import *
+
+class ZOSMCE(object):
+    def __init__(self, TheMCE):
+        self.__TheMCE = TheMCE
+
+    def addConfig(self):
+        pass
+
+    def rmAllConfig(self):
+        pass
+
+    def rmComfig(self, index):
+        pass
